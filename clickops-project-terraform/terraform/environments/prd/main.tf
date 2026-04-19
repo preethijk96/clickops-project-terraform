3d46ab5bb1a570dd7da407604ef03a92820972eb
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-19 18:09:12 +0530

    commit

--- clickops-project-terraform/terraform/environments/prd/main.tf
+++ clickops-project-terraform/terraform/environments/prd/main.tf
@@ -53,7 +53,7 @@ module "ecr" {
 module "secrets" {
   source = "../../modules/secrets"
 
-  secret_name = var.secret_name
+  secret_name   = module.secrets.secret_name
   username    = "admin"
   password    = "password123"
   host        = "mongodb"
