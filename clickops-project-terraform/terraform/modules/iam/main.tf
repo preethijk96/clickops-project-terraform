1eb19d6924733b6b71edf8e1dd622c56ccd5a377
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-22 19:46:28 +0530

    commit

--- clickops-project-terraform/terraform/modules/iam/main.tf
+++ clickops-project-terraform/terraform/modules/iam/main.tf
@@ -1,59 +1,39 @@
 resource "aws_iam_role" "ec2_role" {
-  name = var.role_name
+
+  name = "clickops-ec2-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    Version="2012-10-17"
+    Statement=[
       {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
+        Effect="Allow"
+        Principal={
+          Service="ec2.amazonaws.com"
+        }
+        Action="sts:AssumeRole"
       }
     ]
   })
 }
 
-# Attach multiple policies
-resource "aws_iam_role_policy_attachment" "attach" {
-  for_each = toset(var.policy_arns)
-
+resource "aws_iam_role_policy_attachment" "s3_access" {
   role       = aws_iam_role.ec2_role.name
-  policy_arn = each.value
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-# Instance profile
-resource "aws_iam_instance_profile" "profile" {
-  name = var.role_name
-  role = aws_iam_role.ec2_role.name
-}
-resource "aws_iam_role" "ec2_role" {
-  name = "clickops-ec2-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
+resource "aws_iam_role_policy_attachment" "ecr_access" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn ="arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy" "secrets_policy" {
-  role = aws_iam_role.ec2_role.id
+resource "aws_iam_role_policy_attachment" "ssm_access" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn ="arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "secretsmanager:GetSecretValue"
-      ]
-      Resource = "*"
-    }]
-  })
+resource "aws_iam_instance_profile" "ec2_profile" {
+
+ name = "clickops-instance-profile"
+ role = aws_iam_role.ec2_role.name
+
 }
