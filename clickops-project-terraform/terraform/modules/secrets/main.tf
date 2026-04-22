d25b95f49d620d4a503f337a976ab46f1c594f5e
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-22 21:13:39 +0530

    commit

--- clickops-project-terraform/terraform/modules/secrets/main.tf
+++ clickops-project-terraform/terraform/modules/secrets/main.tf
@@ -3,33 +3,10 @@ resource "aws_secretsmanager_secret" "secret" {
 }
 
 resource "aws_secretsmanager_secret_version" "value" {
-  secret_id     = aws_secretsmanager_secret.secret.id
-  secret_string = jsonencode({
-    username = var.username
-    password = var.password
-    host     = var.host
-    port     = var.port
-  })
-}
-resource "aws_secretsmanager_secret" "mongo" {
-  name = "clickops-sm-dev"
-}
-
-resource "aws_secretsmanager_secret_version" "mongo" {
-  secret_id     = aws_secretsmanager_secret.mongo.id
-  secret_string = jsonencode({
-    host = "mongo"
-    port = 27017
-  })
-}
-resource "aws_secretsmanager_secret" "clickops" {
-  name = "clickops-secret"
-}
-
-resource "aws_secretsmanager_secret_version" "clickops_value" {
-  secret_id = aws_secretsmanager_secret.clickops.id
+  secret_id = aws_secretsmanager_secret.secret.id
 
   secret_string = jsonencode({
-    bucket = var.bucket_name
+    username = "admin"
+    password = "admin123"
   })
 }
--- clickops-project-terraform/terraform/modules/secrets/variables.tf
+++ clickops-project-terraform/terraform/modules/secrets/variables.tf
@@ -1,25 +1,3 @@
 variable "secret_name" {
   type = string
 }
-
-variable "username" {
-  type = string
-}
-
-variable "password" {
-  type = string
-}
-
-variable "host" {
-  type = string
-}
-
-variable "port" {
-  type = number
-}
-variable "environment" {
-  type = string
-}
-variable "bucket_name" {
-  default = "your-s3-bucket-name"
-}
