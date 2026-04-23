2adcbc9cf364eab7986cfbc4837f65736d1d428a
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-24 01:49:52 +0530

    commit

--- clickops-project-terraform/app/backend/app.py
+++ clickops-project-terraform/app/backend/app.py
@@ -1,80 +1,63 @@
-from flask import Flask, request, jsonify
+from flask import Flask,request,jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 import boto3,time,os
 
-app = Flask(__name__)
+app=Flask(__name__)
 CORS(app)
 
-# Mongo
-client = MongoClient("mongodb://mongodb:27017/")
-db = client["clickops"]
-collection = db["records"]
+ENVIRONMENT=os.getenv("ENVIRONMENT")
+BUCKET_NAME=os.getenv("BUCKET_NAME")
+DB_NAME=os.getenv("DB_NAME")
 
-# Environment driven values
-ENV=os.getenv("ENVIRONMENT","dev")
-BUCKET=os.getenv("BUCKET_NAME",f"clickops-bucket-{ENV}")
+client=MongoClient("mongodb://mongodb:27017/")
+db=client[DB_NAME]
+collection=db["records"]
 
-# S3
 s3=boto3.client(
-    "s3",
-    region_name="ap-south-1"
+"s3",
+region_name="ap-south-1"
 )
 
 @app.route("/")
 def home():
-    return jsonify({
-      "message":"Backend running",
-      "environment":ENV
-    })
+ return jsonify({
+   "environment":ENVIRONMENT,
+   "bucket":BUCKET_NAME,
+   "database":DB_NAME
+ })
 
-
-@app.route("/add", methods=["POST"])
+@app.route("/add",methods=["POST"])
 def add_student():
 
-    name=request.form["name"]
-    age=request.form["age"]
-    file=request.files["image"]
+ name=request.form["name"]
+ age=request.form["age"]
+ file=request.files["image"]
 
-    filename=str(int(time.time()))+".jpg"
+ filename=str(int(time.time()))+".jpg"
 
-    file.save("/tmp/"+filename)
+ file.save("/tmp/"+filename)
 
-    s3.upload_file(
-       "/tmp/"+filename,
-       BUCKET,
-       filename
-    )
+ s3.upload_file(
+ "/tmp/"+filename,
+ BUCKET_NAME,
+ filename
+ )
 
-    image_url=f"https://{BUCKET}.s3.ap-south-1.amazonaws.com/{filename}"
+ image_url=f"https://{BUCKET_NAME}.s3.ap-south-1.amazonaws.com/{filename}"
 
-    record={
-       "name":name,
-       "age":age,
-       "image":image_url
-    }
+ student={
+ "name":name,
+ "age":age,
+ "image":image_url,
+ "environment":ENVIRONMENT
+ }
 
-    result=collection.insert_one(record)
+ collection.insert_one(student)
 
-    return jsonify({
-       "message":"Student saved successfully",
-       "id":str(result.inserted_id)
-    })
-
-
-@app.route("/students")
-def students():
-
-    data=[]
-
-    for x in collection.find():
-        x["_id"]=str(x["_id"])
-        data.append(x)
-
-    return jsonify(data)
-
+ return jsonify(student)
 
 app.run(
- host="0.0.0.0",
- port=5000
+host="0.0.0.0",
+port=5000
 )
