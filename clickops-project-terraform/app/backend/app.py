6f546c7d026a21cea731933ffcc101717d75f5c0
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-23 22:04:24 +0530

    commit

--- clickops-project-terraform/app/backend/app.py
+++ clickops-project-terraform/app/backend/app.py
@@ -1,20 +1,12 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
-import boto3,uuid
 
 app=Flask(__name__)
 CORS(app)
 
 mongo=MongoClient("mongodb://mongodb:27017/")
 db=mongo.clickops
-
-bucket="clickops-bucket-dev"
-
-s3=boto3.client(
-"s3",
-region_name="ap-south-1"
-)
 
 @app.route("/")
 def home():
@@ -25,30 +17,24 @@ def add():
     try:
         name=request.form["name"]
         age=request.form["age"]
-        image=request.files["image"]
-
-        filename=str(uuid.uuid4())+"-"+image.filename
-
-        s3.upload_fileobj(
-            image,
-            bucket,
-            filename
-        )
-
-        image_url=f"https://{bucket}.s3.amazonaws.com/{filename}"
 
         db.records.insert_one({
             "name":name,
-            "age":age,
-            "image":image_url
+            "age":age
         })
 
         return jsonify({
-         "message":"Saved in MongoDB and image uploaded to S3"
+            "message":"Student saved successfully"
         })
 
     except Exception as e:
         return jsonify({"error":str(e)}),500
 
+@app.route("/list")
+def list_data():
+    data=[]
+    for x in db.records.find({},{"_id":0}):
+        data.append(x)
+    return jsonify(data)
 
 app.run(host="0.0.0.0",port=5000)
