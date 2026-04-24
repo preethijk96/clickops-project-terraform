2b56004d2b32ace2d1a0bdca4e842eb75b3d8e58
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-24 20:42:31 +0530

    commit

--- clickops-project-terraform/terraform/modules/iam/main.tf
+++ clickops-project-terraform/terraform/modules/iam/main.tf
@@ -1,39 +1,35 @@
 resource "aws_iam_role" "ec2_role" {
-
-  name = "clickops-ec2-role"
+  name = "clickops-iam-role-${var.environment}"
 
   assume_role_policy = jsonencode({
+    Version="2012-10-17"
+    Statement=[{
+      Effect="Allow"
+      Principal={
+        Service="ec2.amazonaws.com"
+      }
+      Action="sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "terraform_policy" {
+  name = "terraform-policy-${var.environment}"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
     Version="2012-10-17"
     Statement=[
       {
         Effect="Allow"
-        Principal={
-          Service="ec2.amazonaws.com"
-        }
-        Action="sts:AssumeRole"
+        Action="*"
+        Resource="*"
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "s3_access" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_access" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn ="arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_access" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn ="arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-
- name = "clickops-instance-profile"
- role = aws_iam_role.ec2_role.name
-
+resource "aws_iam_instance_profile" "profile" {
+  name = "clickops-role-${var.environment}"
+  role = aws_iam_role.ec2_role.name
 }
