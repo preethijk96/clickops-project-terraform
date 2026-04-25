0b1628fd16ac35ee14eb2777db66e90d02805e01
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-25 20:36:24 +0530

    commit

--- clickops-project-terraform/terraform/environments/prd/main.tf
+++ clickops-project-terraform/terraform/environments/prd/main.tf
@@ -1,38 +1,34 @@
 module "iam" {
   source = "../../modules/iam"
 
-  role_name = "clickops-role-${var.environment}"
-
-  policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
-  ]
+  environment = var.environment
 }
 
 module "s3" {
   source = "../../modules/s3"
 
   bucket_name = "clickops-bucket-${var.environment}"
-  environment = var.environment
 }
 
 module "ecr" {
   source = "../../modules/ecr"
 
-  repo_name = "clickops-ecr-${var.environment}"
+  ecr_name = "clickops-ecr-${var.environment}"
 }
 
 module "secrets" {
   source = "../../modules/secrets"
 
   secret_name = "mongo-creds-${var.environment}"
+  environment = var.environment
 }
 
 module "ec2" {
   source = "../../modules/ec2"
 
+  instance_name     = "clickops-ec2-${var.environment}"
   environment       = var.environment
+
   ami               = var.ami
   instance_type     = var.instance_type
   key_name          = var.key_name
@@ -40,4 +36,6 @@ module "ec2" {
 
   frontend_port     = var.frontend_port
   backend_port      = var.backend_port
+
+  vpc_id            = var.vpc_id
 }
--- clickops-project-terraform/terraform/environments/prd/outputs.tf
+++ clickops-project-terraform/terraform/environments/prd/outputs.tf
@@ -1,6 +1,3 @@
-output "public_ip" {
+output "instance_public_ip" {
   value = module.ec2.public_ip
-}
-output "instance_profile" {
- value = aws_iam_instance_profile.profile.name
 }
