5bc5ab4a017ce4dea7e6100bcbe45845d717c24a
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-25 18:08:11 +0530

    commit

--- clickops-project-terraform/terraform/environments/qa/main.tf
+++ clickops-project-terraform/terraform/environments/qa/main.tf
@@ -1,35 +1,39 @@
 module "iam" {
- source = "../iam"
- environment = var.environment
+  source = "../../modules/iam"
+
+  environment = var.environment
 }
 
 module "s3" {
- source = "../s3"
- bucket_name = "clickops-bucket-${var.environment}"
- environment = var.environment
+  source = "../../modules/s3"
+
+  bucket_name = "clickops-bucket-${var.environment}"
+  environment = var.environment
 }
 
 module "ecr" {
- source = "../ecr"
- ecr_name = "clickops-ecr-${var.environment}"
- environment = var.environment
+  source = "../../modules/ecr"
+
+  ecr_name    = "clickops-ecr-${var.environment}"
+  environment = var.environment
 }
 
 module "secrets" {
- source = "../secrets"
- secret_name = "mongo-creds-${var.environment}"
- environment = var.environment
+  source = "../../modules/secrets"
+
+  secret_name = "mongo-creds-${var.environment}"
+  environment = var.environment
 }
 
 module "ec2" {
- source = "../ec2"
+  source = "../../modules/ec2"
 
- environment       = var.environment
- ami               = var.ami
- instance_type     = var.instance_type
- key_name          = var.key_name
- root_volume_size  = var.root_volume_size
+  environment      = var.environment
+  ami              = var.ami
+  instance_type    = var.instance_type
+  key_name         = var.key_name
+  root_volume_size = var.root_volume_size
 
- frontend_port     = var.frontend_port
- backend_port      = var.backend_port
+  frontend_port    = var.frontend_port
+  backend_port     = var.backend_port
 }
