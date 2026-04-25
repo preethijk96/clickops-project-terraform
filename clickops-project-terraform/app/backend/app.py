747ef56773f764fd5fa77dcc28cdf97d029d7f51
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-25 15:26:39 +0530

    commit

--- clickops-project-terraform/app/backend/app.py
+++ clickops-project-terraform/app/backend/app.py
@@ -11,15 +11,13 @@ CORS(app)
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 BUCKET_NAME = os.getenv("BUCKET_NAME")
 DB_NAME = os.getenv("DB_NAME")
+MONGO_HOST = os.getenv("MONGO_HOST")
 
-client = MongoClient("mongodb://mongodb-dev:27017/")
+client = MongoClient(f"mongodb://{MONGO_HOST}:27017/")
 db = client[DB_NAME]
 collection = db["records"]
 
-s3 = boto3.client(
-    "s3",
-    region_name="ap-south-1"
-)
+s3 = boto3.client("s3", region_name="ap-south-1")
 
 @app.route("/")
 def home():
@@ -29,16 +27,13 @@ def home():
         "database": DB_NAME
     })
 
-
 @app.route("/add", methods=["POST"])
 def add_student():
-
     name = request.form["name"]
     age = request.form["age"]
     file = request.files["image"]
 
     filename = str(int(time.time())) + ".jpg"
-
     file.save("/tmp/" + filename)
 
     s3.upload_file(
@@ -58,10 +53,20 @@ def add_student():
 
     collection.insert_one(student)
 
-    return jsonify(student)
+    return jsonify({
+        "message":"Student added successfully",
+        **student
+    })
 
+@app.route("/students")
+def students():
+    users=[]
+    for x in collection.find({},{"_id":0}):
+        users.append(x)
+    return jsonify(users)
 
-app.run(
-    host="0.0.0.0",
-    port=5000
-)
+if __name__=="__main__":
+    app.run(
+      host="0.0.0.0",
+      port=5000
+    )
