7f33f9954d9834fbdbcd699ce27d527db2e2192e
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-04-25 20:27:49 +0530

    commit

--- clickops-project-terraform/terraform/environments/prd/main.tf
+++ clickops-project-terraform/terraform/environments/prd/main.tf
@@ -1,37 +1,43 @@
 module "iam" {
- source = "../iam"
- environment = var.environment
+  source = "../../modules/iam"
+
+  role_name = "clickops-role-${var.environment}"
+
+  policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+  ]
 }
 
 module "s3" {
- source = "../s3"
- bucket_name = "clickops-bucket-${var.environment}"
- environment = var.environment
+  source = "../../modules/s3"
+
+  bucket_name = "clickops-bucket-${var.environment}"
+  environment = var.environment
 }
 
 module "ecr" {
- source = "../ecr"
- ecr_name = "clickops-ecr-${var.environment}"
- environment = var.environment
+  source = "../../modules/ecr"
+
+  repo_name = "clickops-ecr-${var.environment}"
 }
 
 module "secrets" {
- source = "../secrets"
- secret_name = "mongo-creds-${var.environment}"
- environment = var.environment
+  source = "../../modules/secrets"
+
+  secret_name = "mongo-creds-${var.environment}"
 }
 
 module "ec2" {
   source = "../../modules/ec2"
 
-  ami              = var.ami
-  instance_type    = var.instance_type
-  key_name         = var.key_name
-  instance_name    = "clickops-ec2-${var.environment}"
-  root_volume_size = var.root_volume_size
+  environment       = var.environment
+  ami               = var.ami
+  instance_type     = var.instance_type
+  key_name          = var.key_name
+  root_volume_size  = var.root_volume_size
 
-  vpc_id           = module.vpc.vpc_id
-  subnet_id = module.vpc.subnet_id
-  instance_profile = module.iam.instance_profile
-  sg_name          = "clickops-sg-${var.environment}"
+  frontend_port     = var.frontend_port
+  backend_port      = var.backend_port
 }
