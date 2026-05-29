6dc1fa3376c65ab9c82927eeb897034f480e0a2a
preethijk96 <preethijayaprasad.1996@gmail.com>
2026-05-29 12:26:38 +0530

    Implemented fully automated DEV QA PRD CI/CD pipeline
    - Added dev.yml workflow
    - Added qa.yml workflow
    - Added prd.yml workflow
    - Removed old deploy.yml
    - Added SSM permissions to IAM module
    - Added ECR permissions to IAM modules

--- clickops-project-terraform/terraform/modules/iam/main.tf
+++ clickops-project-terraform/terraform/modules/iam/main.tf
@@ -1,24 +1,34 @@
 resource "aws_iam_role" "ec2_role" {
- name = "clickops-role-${var.environment}"
+  name = "clickops-role-${var.environment}"
 
- assume_role_policy = jsonencode({
- Version="2012-10-17"
- Statement=[{
-   Effect="Allow"
-   Principal={
-      Service="ec2.amazonaws.com"
-   }
-   Action="sts:AssumeRole"
- }]
- })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "admin" {
- role = aws_iam_role.ec2_role.name
- policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 resource "aws_iam_instance_profile" "profile" {
- name = "clickops-profile-${var.environment}"
- role = aws_iam_role.ec2_role.name
+  name = "clickops-profile-${var.environment}"
+  role = aws_iam_role.ec2_role.name
 }
